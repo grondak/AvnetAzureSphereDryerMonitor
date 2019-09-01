9956688f0056f04211f7d7dbb03e1f579d0e9742
Tony Reynolds <grondak@gmail.com>
2019-09-01 10:20:56 -0400

    Tony claims the forked repo instead of the cloned repo

--- AvnetStarterKitReferenceDesign/connection_strings.h
+++ AvnetStarterKitReferenceDesign/connection_strings.h
@@ -1,4 +1,4 @@
 #pragma once
 
 // Define your connection string here.  The connection string is required to connect to Azure.
-#define MY_CONNECTION_STRING ""
+#define MY_CONNECTION_STRING "HostName=iotc-9212fb2b-74b1-4bcf-bd3f-856af04040e4.azure-devices.net;DeviceId=09c0c712-3beb-4ac9-a9d0-0e9d2cf82a15;SharedAccessKey=f2t8NJVm61aHMp4Z+ze2ahiieqS0o4qz2qG4cGwL1I8="
